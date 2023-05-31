82059393821714b5711d2e514368baf90198fdf1
liuzhaoze <1045954863@qq.com>
2023-05-31 08:53:11 +0000

    add comments

--- lib/Basket.cpp
+++ lib/Basket.cpp
@@ -29,7 +29,9 @@ double Basket::print_receipt(std::ostream &os) const
 {
     double sum = 0.0;
 
-    // upper_bound 每次指向下一个关键字不同的元素（也是具有相同关键字元素的尾后位置）
+    // upper_bound 每次指向下一个关键字不同的元素（也就是具有相同关键字元素子序列的尾后位置）
+    // 通过对相邻两个元素调用 Basket::compare 来判断关键字是否相同
+    // 返回值为 False 说明关键字相同，返回值为 True 说明关键字不同
     for (auto iter = items.cbegin(); iter != items.cend(); iter = items.upper_bound(*iter))
     {
         sum += print_total(os, **iter, items.count(*iter)); // items 中的每个元素代表一本书
